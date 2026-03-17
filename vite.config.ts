5a422b2cbcc34d0ae67cce7535ae62b8fec63dd1
Ned Nin <jed.lin1015@gmail.com>
2026-03-17 16:52:28 -0400

    try to unbreak

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/',
+  base: './',
   server: {
     https: {
       key: './localhost+1-key.pem',
