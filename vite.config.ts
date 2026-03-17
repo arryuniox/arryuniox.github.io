13209987ea46681a64c8c77d29d2282a538c1bae
Ned Nin <jed.lin1015@gmail.com>
2026-03-17 19:40:32 -0400

    why does it work on local but not when i deploy im gonna tweak

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
-function mimeTypePlugin() {
-  return {
-    name: 'fix-mime-types',
-    apply: 'serve' as const,
-    configureServer(server: any) {
-      return () => {
-        server.middlewares.use((req: any, res: any, next: any) => {
-          if (req.url) {
-            if (req.url.endsWith('.ts') || req.url.endsWith('.tsx') || 
-                req.url.endsWith('.js') || req.url.endsWith('.jsx')) {
-              res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
-            } else if (req.url.endsWith('.css')) {
-              res.setHeader('Content-Type', 'text/css; charset=utf-8');
-            }
-          }
-          next();
-        });
-      };
-    },
-  };
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mimeTypePlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -39,5 +17,8 @@ export default defineConfig({
     //   cert: './localhost+1.pem',
     // },
     host: '0.0.0.0',
+    fs: {
+      strict: false,
+    },
   },
 })
